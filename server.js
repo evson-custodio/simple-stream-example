b54f5e25c33029efee5d1f2a9fddfe7c90a67d71
evsoncustodio <evsoncustodio@gmail.com>
2018-10-01 15:32:32 -0300

    Removed IP Address Config

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const debug = require('debug')('simple-stream-example:server');
 
 const http = require('http');
 
-const address = process.env.PUBLIC_IP_ADDRESS || '127.0.0.1'
+// const address = process.env.PUBLIC_IP_ADDRESS || '127.0.0.1'
 const port = process.env.PORT || 1337;
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/env')[env];
 
-function configureServer(address, port) {
+function configureServer(port) {
     require('./api')(config)
     .then(api => {
         const app = require('./config/express')(api, port);
         const server = http.createServer(app);
 
-        server.listen(app.get('port'), address, () => {
+        server.listen(app.get('port'), () => {
             debug('Server Running on http://' + server.address().address + ':' + server.address().port);
         });
     })
@@ -23,6 +23,6 @@ function configureServer(address, port) {
     });
 }
 
-configureServer(address, port);
+configureServer(port);
 
 module.exports = configureServer;
